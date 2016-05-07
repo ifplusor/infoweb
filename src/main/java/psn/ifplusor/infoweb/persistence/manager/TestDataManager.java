5e4d79d7a8a25e1b9c58fdc48afdc679e2f642bb
James Yin <ywhjames@hotmail.com>
2016-05-07 10:52:28 +0800

    更新:使用JPA取代纯hibernate

--- src/main/java/psn/ifplusor/infoweb/persistence/manager/TestDataManager.java
+++ src/main/java/psn/ifplusor/infoweb/persistence/manager/TestDataManager.java
@@ -2,23 +2,33 @@ package psn.ifplusor.infoweb.persistence.manager;
 
 import java.util.List;
 
-import org.hibernate.Query;
-import org.hibernate.Session;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+//import org.hibernate.Query;
+//import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import psn.ifplusor.core.hibernate.HibernateEntityDao;
+//import psn.ifplusor.core.hibernate.HibernateEntityDao;
 import psn.ifplusor.infoweb.persistence.domain.TestData;
 
 @Repository
-public class TestDataManager extends HibernateEntityDao{
+//public class TestDataManager extends HibernateEntityDao {
+public class TestDataManager {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	@Transactional
 	public List<TestData> getAllData() {
 		String hql = "from TestData";
-		Session session = getSession();
-		Query query = session.createQuery(hql);
+//		Session session = getSession();
+//		Query query = session.createQuery(hql);
+		Query query = entityManager.createQuery(hql);
 		
-		return query.list();
+		return query.getResultList();
+		//return query.list();
 	}
 }
